8acdb74699f75b78eec9b75019a1e522e50b7754
Kale <2503491@cognizant.com>
2026-06-30 00:10:39 +0530

    Added Customer Repo

--- customer-service/src/main/java/com/genc/customer_service/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/genc/customer_service/repository/CustomerRepository.java
@@ -1,4 +1,12 @@
 package com.genc.customer_service.repository;
 
-public interface CustomerRepository {
+import com.genc.customer_service.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
 }
